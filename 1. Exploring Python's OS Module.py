1a6ec61ee66f53993a928a7bb51c231fb41652a5
Marko <markogidej@yahoo.com>
2024-04-04 17:16:29 -0400

    Init Commit

---
+++ 1. Exploring Python's OS Module.py
@@ -0,0 +1,12 @@
+# Task 1: Directory Inspector:
+
+
+
+
+# Task 2: File Size Reporter:
+
+
+
+
+# Task 3: File Extension Counter:
+
---
+++ 3. Advanced Python Data Processing and Analysis Challenge.py
@@ -0,0 +1,7 @@
+# Task 1: Travel Blog Sentiment Analysis:
+
+
+
+
+# Task 2: Historical Weather Data Compiler
+
