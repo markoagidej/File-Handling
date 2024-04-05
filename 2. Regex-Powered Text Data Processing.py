756cc8a239a770d0ffd94ea6062dc7560ee9c383
Marko <markogidej@yahoo.com>
2024-04-05 16:29:05 -0400

    Answered Question 2

--- 2. Regex-Powered Text Data Processing.py
+++ 2. Regex-Powered Text Data Processing.py
@@ -1,2 +1,20 @@
 # Task 1: Email Extractor:
 
+import re
+
+def extract_emails(filename):
+    email_list = set()
+        
+    with open(filename, "r") as file:
+        email_list = []
+        for line in file:
+            strip_line = line.strip()
+            print(strip_line)
+            emails = re.findall(r"[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}", strip_line)
+            if emails:
+                email_list.append(emails)
+
+    print(email_list)
+
+
+extract_emails("contacts.txt")
